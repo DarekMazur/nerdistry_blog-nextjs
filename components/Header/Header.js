c6eb0fd725e69840b4deaab9c5b5eb46aceba1cf
Darek Mazur <dev@darekmazur.it>
2021-11-11 19:26:12 +0100

    add header sketch

---
+++ components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const Header = () => {
+  return (
+    <Head>
+      <title>Lorem ipsum</title>
+      <meta name="description" content="" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+    </Head>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
 import Head from 'next/head';
 // import Image from 'next/image';
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
-      <Head>
-        <title>Lorem ipsum</title>
-        <meta name="description" content="" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-
       <main>
         <h1>Hello world</h1>
       </main>
@@ -17,4 +11,6 @@ export default function Home() {
       <footer></footer>
     </div>
   );
-}
+};
+
+export default Home;

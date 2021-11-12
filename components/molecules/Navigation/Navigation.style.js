ae6be79c4cf626a5d5a3bb52d80efce38480ad31
Darek Mazur <dev@darekmazur.it>
2021-11-12 12:24:50 +0100

    animate navigation on scroll

--- components/molecules/Navigation/Navigation.style.js
+++ components/molecules/Navigation/Navigation.style.js
@@ -7,22 +7,29 @@ export const NavigationWrapper = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: ${({ isScroll }) => (isScroll ? '50px' : '91px')};
+  height: 91px;
   background-color: ${({ theme }) => theme.colors.white};
   padding: 0 52px;
   box-shadow: 0 0.2rem 1rem ${({ theme }) => theme.colors.black};
   z-index: 10;
+  transform: translateY(${({ isScroll }) => (isScroll ? '-41px' : '0')});
+  transition: transform 0.2s ease-in-out;
 `;
 
 export const StyledNavigation = styled.nav``;
 
 export const NavigationList = styled.ul`
   display: flex;
+  height: 100%;
   justify-content: flex-end;
+  align-items: ${({ isScroll }) => (isScroll ? 'flex-end' : 'center')};
   list-style: none;
   font-family: ${({ theme }) => theme.fonts.headerFont};
   font-size: ${({ theme, isScroll }) => (isScroll ? theme.fontSize.m : theme.fontSize.l)};
   font-weight: 700;
+  padding: ${({ isScroll }) => (isScroll ? '0 0 12px' : '0')};
+  margin: 0;
+  transition: font-size 0.2s ease-in-out;
 `;
 
 export const NavigationListItem = styled.li`
@@ -31,21 +38,27 @@ export const NavigationListItem = styled.li`
 
 export const Branding = styled.div`
   position: absolute;
-  top: ${({ isScroll }) => (isScroll ? '3px' : '25px')};
+  top: 0;
   left: 52px;
   display: flex;
   align-items: center;
+  transform: translateY(${({ isScroll }) => (isScroll ? '15px' : '25px')});
+  transition: transform 0.2s ease-in-out;
 `;
 
 export const LogoWrapper = styled.div`
   position: relative;
-  width: ${({ isScroll }) => (isScroll ? '40px' : '100px')};
-  height: ${({ isScroll }) => (isScroll ? '40px' : '100px')};
+  width: 100px;
+  height: 100px;
+  transform: scale(${({ isScroll }) => (isScroll ? '0.4' : '1')});
+  transition: transform 0.2s ease-in-out;
   background-color: ${({ theme }) => theme.colors.white};
 `;
 
 export const SiteTitle = styled.h1`
   font-size: ${({ theme, isScroll }) => (isScroll ? theme.fontSize.m : theme.fontSize.l)};
   font-family: ${({ theme }) => theme.fonts.brandingFont};
+  transform: translateX(${({ isScroll }) => (isScroll ? '-35px' : '0')});
   padding-left: 1rem;
+  transition: font-size 0.2s ease-in-out, transform 0.2s ease-in-out;
 `;

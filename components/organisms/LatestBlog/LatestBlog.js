aa30aee1adf40de97b669f474008f19b0cfddbba
Darek Mazur <dev@darekmazur.it>
2021-11-12 15:40:54 +0100

    create fake content for latest blog

--- components/organisms/LatestBlog/LatestBlog.js
+++ components/organisms/LatestBlog/LatestBlog.js
@@ -10,19 +10,30 @@ const LatestBlog = () => {
         {/* post item start */}
         <div>
           <div>
-            <h4>Post title</h4>
-            <p>Publish date</p>
+            <h4>Lorem Ipsum</h4>
+            <p>01.01.2021</p>
           </div>
           <div>
-            <div>image</div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1633113212423-dc2828784c7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+                alt=""
+              />
+            </div>
             <div>
               <div>
-                <p>Post short</p>
-                <p>Reading time</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+                  erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                  est Lorem ipsum dolor sit amet. Lorem ipsum.
+                </p>
+                <p>Read in 3 minutes</p>
               </div>
               <div>
-                <div>Author's photo</div>
-                <p>Author</p>
+                <div>
+                  <img src="/image.jpg" alt="" />
+                </div>
+                <p>John Doe</p>
               </div>
               <button>Read more</button>
             </div>

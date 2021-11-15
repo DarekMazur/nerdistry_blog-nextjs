87f5c5d6c7f94e24ea9dbd24bd8dc1b29baa650f
Darek Mazur <dev@darekmazur.it>
2021-11-15 19:45:26 +0100

    create getNumberOfPosts method

--- pages/blog.js
+++ pages/blog.js
@@ -3,13 +3,15 @@ import PostsList from '../components/organisms/PostsList/PostsList';
 
 export const BlogContext = React.createContext({
   allPosts: [],
+  numberOfPosts: 0,
 });
 
-const Blog = ({ allPosts }) => {
+const Blog = ({ allPosts, numberOfPosts }) => {
   return (
     <BlogContext.Provider
       value={{
         allPosts,
+        numberOfPosts,
       }}
     >
       <PostsList isBlog />
@@ -18,12 +20,16 @@ const Blog = ({ allPosts }) => {
 };
 
 export async function getStaticProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/posts?_sort=published_at:DESC`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/posts?_sort=published_at:DESC&_limit=5`);
   const allPosts = await res.json();
+
+  const getNumberOfPosts = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/posts/count`);
+  const numberOfPosts = await getNumberOfPosts.json();
 
   return {
     props: {
       allPosts,
+      numberOfPosts,
     },
   };
 }

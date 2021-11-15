3274ed73f61e25fcf9a9ce2abaea8ff2352bb0ca
Darek Mazur <dev@darekmazur.it>
2021-11-15 20:24:00 +0100

    create infinite scroll

--- components/organisms/PostsList/PostsList.js
+++ components/organisms/PostsList/PostsList.js
@@ -1,28 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { PostsContext } from '../../../pages';
 import { BlogContext } from '../../../pages/blog';
 import PostsListItem from '../../molecules/PostsListItem/PostsListItem';
 import { PostListWrapper } from './PostsList.style';
 
 const PostsList = ({ isBlog }) => {
-  const { posts, allPosts } = useContext(isBlog ? BlogContext : PostsContext);
+  const { posts, blogPosts, numberOfPosts } = useContext(isBlog ? BlogContext : PostsContext);
+  const postsToDisplay = isBlog ? blogPosts : posts;
 
-  const postsToDisplay = isBlog ? allPosts : posts;
+  const [postsList, setPostList] = useState([...postsToDisplay]);
+  const [hasMore, setHasMore] = useState(true);
+
+  const getMorePosts = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/posts?_sort=published_at:DESC&_start=${postsList.length}&_limit=5`);
+    const newPosts = await res.json();
+    setPostList((postsList) => [...postsList, ...newPosts]);
+  };
+
+  useEffect(() => {
+    setHasMore(numberOfPosts > postsList.length);
+  }, [postsList]);
 
   return (
     <PostListWrapper>
-      {postsToDisplay.map(({ id, Title, Description, Content, published_at, CoverImage, MainCategory, SecondaryCategory, Tags }) => (
-        <PostsListItem
-          key={id}
-          title={Title}
-          content={Content}
-          publishdate={published_at}
-          photo={CoverImage.url}
-          category={[MainCategory, SecondaryCategory]}
-          description={Description}
-          tags={Tags}
-        />
-      ))}
+      <InfiniteScroll dataLength={postsList.length} next={getMorePosts} hasMore={hasMore} loader={'Loading...'} endMessage={`That's all :)`}>
+        {console.log(`numberOfPosts: ${numberOfPosts}, postsList.length: ${postsList.length}, hasMore: ${hasMore}`)}
+        {postsList.map(({ id, Title, Description, Content, published_at, CoverImage, MainCategory, SecondaryCategory, Tags }) => (
+          <PostsListItem
+            key={id}
+            title={Title}
+            content={Content}
+            publishdate={published_at}
+            photo={CoverImage.url}
+            category={[MainCategory, SecondaryCategory]}
+            description={Description}
+            tags={Tags}
+          />
+        ))}
+      </InfiniteScroll>
     </PostListWrapper>
   );
 };
--- pages/blog.js
+++ pages/blog.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PostsList from '../components/organisms/PostsList/PostsList';
 
 export const BlogContext = React.createContext({
-  allPosts: [],
+  blogPosts: [],
   numberOfPosts: 0,
 });
 
-const Blog = ({ allPosts, numberOfPosts }) => {
+const Blog = ({ blogPosts, numberOfPosts }) => {
   return (
     <BlogContext.Provider
       value={{
-        allPosts,
+        blogPosts,
         numberOfPosts,
       }}
     >
@@ -21,14 +21,14 @@ const Blog = ({ allPosts, numberOfPosts }) => {
 
 export async function getStaticProps() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/posts?_sort=published_at:DESC&_limit=5`);
-  const allPosts = await res.json();
+  const blogPosts = await res.json();
 
   const getNumberOfPosts = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/posts/count`);
   const numberOfPosts = await getNumberOfPosts.json();
 
   return {
     props: {
-      allPosts,
+      blogPosts,
       numberOfPosts,
     },
   };

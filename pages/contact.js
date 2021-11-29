1cb5bc2b1e1cde2cab54e701f88e14981e522849
Darek Mazur <dev@darekmazur.it>
2021-11-29 21:04:38 +0100

    create contact page sketch component

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <form>
+      <input></input>
+      <input></input>
+      <input></input>
+      <button>Send</button>
+    </form>
+  );
+};
+
+export default Contact;

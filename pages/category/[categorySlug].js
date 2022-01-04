903044f2de7f83559c8f8f832e8120438c198ff3
Darek Mazur <dev@darekmazur.it>
2022-01-04 08:34:28 +0100

    refactor category description

--- pages/category/[categorySlug].js
+++ pages/category/[categorySlug].js
@@ -2,8 +2,6 @@ import { useRouter } from 'next/router';
 import React, { useContext, useEffect } from 'react';
 import slugify from 'slugify';
 import { CategoryDescription } from '../../components/atoms/CategoryDescription/CategoryDescription';
-import { Slogan } from '../../components/atoms/Slogan/Slogan.style';
-import { TitleH4 } from '../../components/atoms/TitleH4/TitleH4.style';
 import SectionTitle from '../../components/molecules/SectionTitle/SectionTitle';
 import PostsList from '../../components/organisms/PostsList/PostsList';
 import { ContentContext } from '../../providers/ContentProvider';

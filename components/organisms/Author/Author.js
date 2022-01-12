c4b17151c5f8d24f6c614fbc8f8b2415e1f577a2
Darek Mazur <dev@darekmazur.it>
2022-01-12 23:59:17 +0100

    handle author empty avatar error

--- components/organisms/Author/Author.js
+++ components/organisms/Author/Author.js
@@ -3,6 +3,7 @@ import Markdown from 'react-markdown';
 import { ContentContext } from '../../../providers/ContentProvider';
 import SectionTitle from '../../molecules/SectionTitle/SectionTitle';
 import { AuthorWrapper, Avatar, CloseButton } from './Author.style';
+import avatar from '../../../assets/images/svg/avatar.svg';
 
 const Author = ({ author }) => {
   const { isOpen, handleClick } = useContext(ContentContext);
@@ -10,7 +11,7 @@ const Author = ({ author }) => {
   return (
     <AuthorWrapper isOpen={isOpen}>
       <CloseButton icon={['fas', 'times']} onClick={handleClick} />
-      <Avatar src={author?.Avatar.url} alt="" />
+      <Avatar src={author?.Avatar ? author?.Avatar.url : avatar.src} alt="" />
       <SectionTitle title={author?.username} />
       <Markdown children={author?.Bio} />
       <p>{author?.email}</p>

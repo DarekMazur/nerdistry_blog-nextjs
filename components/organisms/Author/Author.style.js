56fc4f137bacef91adcdbe1ca11557c3876caf8c
Darek Mazur <dev@darekmazur.it>
2022-01-13 15:06:59 +0100

    style author for small mobile

--- components/organisms/Author/Author.style.js
+++ components/organisms/Author/Author.style.js
@@ -18,6 +18,10 @@ export const AuthorWrapper = styled.div`
   @media (max-width: 1280px) {
     width: 50vw;
   }
+
+  @media (max-width: 767px) {
+    width: 100vw;
+  }
 `;
 
 export const CloseButton = styled(FontAwesomeIcon)`

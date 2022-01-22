7952db42ab5000f3bf705d357e288816e41c13cf
Darek Mazur <dev@darekmazur.it>
2022-01-22 21:56:41 +0100

    clear unused comments

--- components/organisms/Searchbar/Searchbar.js
+++ components/organisms/Searchbar/Searchbar.js
@@ -1,13 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Formik, useFormikContext } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import Input from '../../atoms/Input/Input';
 import { SearchButton, SearchIconWrapper, SearchWrapper } from './SearchBar.style';
 import { ErrorMessage } from '../../atoms/Input/Input.style';
 import { useRouter } from 'next/dist/client/router';
-// import { ContentContext } from '../../../providers/ContentProvider';
 
 const validationSchema = Yup.object().shape({
   search: Yup.string().required('hey, tell me first what you are looking for!'),
@@ -15,11 +14,6 @@ const validationSchema = Yup.object().shape({
 
 const SearchBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  // const [categories, setCategories] = useState([]);
-  // const [initialValues, setInitialValues] = useState({});
-  // const [values, setValues] = useState({});
-
-  // const { getCategoriesPosts } = useContext(ContentContext);
 
   const router = useRouter();
 
@@ -31,41 +25,6 @@ const SearchBar = () => {
     const error = message ? <ErrorMessage>{message}</ErrorMessage> : null;
     return error;
   };
-
-  // const getCategoriesList = async () => {
-  //   const resCat = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/categories`);
-  //   const getAllCategories = await resCat.json();
-
-  //   setCategories(() => [...getAllCategories]);
-  // };
-
-  // const FormikOnChange = () => {
-  //   const { values } = useFormikContext();
-  //   setValues(values);
-  //   console.log(values);
-
-  //   return null;
-  // };
-
-  // useEffect(() => {
-  //   for (const [key, value] of Object.entries(values)) {
-  //     if (value === true) {
-  //       categories.push(key);
-  //     }
-  //   }
-  //   getCategoriesPosts(categories.join('&_categories.Name='));
-  // }, [values]);
-
-  // useEffect(() => {
-  //   getCategoriesList();
-  // }, []);
-
-  // useEffect(() => {
-  //   categories.forEach((category) => {
-  //     const key = category.Name;
-  //     setInitialValues((prevState) => ({ ...prevState, [key]: true }));
-  //   });
-  // }, [categories]);
 
   return (
     <SearchWrapper isOpen={isOpen}>
@@ -101,28 +60,6 @@ const SearchBar = () => {
           </form>
         )}
       </Formik>
-      {/* <Formik initialValues={initialValues} enableReinitialize={true}>
-        {({ values, handleChange }) => (
-          <form>
-            <p>Filter</p>
-            <FormikOnChange />
-            {categories.length > 0
-              ? categories.map((category) => (
-                  <Input
-                    key={category.id}
-                    type="checkbox"
-                    name={category.Name}
-                    id={category.Name}
-                    label={category.Name}
-                    isChecked={values[category.Name]}
-                    onChange={handleChange}
-                    value={values[category.Name]}
-                  />
-                ))
-              : null}
-          </form>
-        )}
-      </Formik> */}
       <SearchIconWrapper onClick={handleClick}>
         <FontAwesomeIcon icon={['fas', 'search']} />
       </SearchIconWrapper>

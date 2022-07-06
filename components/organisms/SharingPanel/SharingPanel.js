6a240749d78e1a0da59958547bd09cf9b8139aea
Darek Mazur <dev@darekmazur.it>
2022-07-06 10:08:50 +0200

    style sharing buttons

---
+++ components/organisms/SharingPanel/SharingPanel.js
@@ -0,0 +1,47 @@
+import { useRouter } from 'next/router';
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  FacebookMessengerShareButton,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from 'next-share';
+import { StyledSharing } from './SharingPanel.style';
+
+const SharingPanel = ({ title }) => {
+  const router = useRouter();
+  const link = `${process.env.NEXT_PUBLIC_URL}${router.asPath}`;
+
+  return (
+    <StyledSharing>
+      <h4>Podziel się:</h4>
+      <FacebookShareButton url={link} quote={title}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+      <TwitterShareButton url={link} title={title}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+      <RedditShareButton url={link} title={title}>
+        <RedditIcon size={32} round />
+      </RedditShareButton>
+      <LinkedinShareButton url={link}>
+        <LinkedinIcon size={32} round />
+      </LinkedinShareButton>
+      <FacebookMessengerShareButton url={link}>
+        <FacebookMessengerIcon size={32} round />
+      </FacebookMessengerShareButton>
+      <EmailShareButton url={link} subject={title}>
+        <EmailIcon size={32} round />
+      </EmailShareButton>
+    </StyledSharing>
+  );
+};
+
+export default SharingPanel;
---
+++ components/organisms/SharingPanel/SharingPanel.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledSharing = styled.div`
+  margin-top: 3rem;
+
+  h4 {
+    margin-bottom: 0.5rem;
+  }
+
+  button {
+    margin: 0.5rem;
+  }
+`;

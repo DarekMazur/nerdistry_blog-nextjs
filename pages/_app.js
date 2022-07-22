2e5287bdc34c60ad6c882b98b4a3eb1506bbf1b6
Darek Mazur <dev@darekmazur.it>
2022-07-22 12:36:59 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
 import SimpleReactLightbox from 'simple-react-lightbox';
 import { ThemeProvider } from 'styled-components';
-
 import Layout from '../components/templates/Layout/Layout';
 import ContentPovider from '../providers/ContentProvider';
 import { GlobalStyle } from '../styles/globalStyle';
 import { theme } from '../utils/theme';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <GlobalStyle />
       <ContentPovider allPosts={pageProps.posts} postsCountValue={pageProps.postsCount}>
         <SimpleReactLightbox>
